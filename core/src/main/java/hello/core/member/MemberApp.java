fcc01a719c0c0fca13ec99e8ddc48c2af3a66506
devhoody <rethnoly@gmail.com>
2023-12-07 21:04:09 +0900

    feat: memberService 구현(MemoryMemberRepository)

---
+++ core/src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+
+public class MemberApp {
+    public static void main(String[] args) {
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getName());
+        System.out.println("new member = " + member.getName());
+    }
+}
---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    // 생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+
+        return memberRepository.findById(memberId);
+    }
+}
