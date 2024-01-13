1476a498ca2f696f5f81d767fb5d221a7cb4dd98
devhoody <rethnoly@gmail.com>
2024-01-13 16:41:45 +0900

    feat: item 클래스 수량 오타 수정
    quentity -> quantity

--- springmvc/item-service/src/main/java/hello/itemservice/domain/Item.java
+++ springmvc/item-service/src/main/java/hello/itemservice/domain/Item.java
@@ -9,14 +9,14 @@ public class Item {
     private Long id;
     private String name;
     private Integer price;
-    private Integer quentity;
+    private Integer quantity;
 
     public Item() {
     }
 
-    public Item(String name, Integer price, Integer quentity) {
+    public Item(String name, Integer price, Integer quantity) {
         this.name = name;
         this.price = price;
-        this.quentity = quentity;
+        this.quantity = quantity;
     }
 }
