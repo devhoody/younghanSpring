ebe7c8000135b4d32b9d821fe2a8bf752c580c36
devhoody <rethnoly@gmail.com>
2024-03-12 21:20:15 +0900

    feat: TimeTraceApp 빈 등록 방법 변경 - 직접 등록
    SpringConfig에 @Bean 등록
    TimeTraceApp.java
    - @Around를 통한 AOP 대상 변경 - SpringConfig 제외

--- hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -5,10 +5,9 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+@Aspect // aop사용 애노테이션
 public class TimeTraceAop {
-    @Around("execution(* hello.hellospring..*(..))")
+    @Around("execution(* hello.hellospring..*(..)) &&  !target(hello.hellospring.SpringConfig)") // 패키지 하위에 모두 적용하라는 뜻 (hello.hellostrping 하위 클래스 전부)
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
         long start = System.currentTimeMillis();
         System.out.println("START :" + joinPoint.toString());
